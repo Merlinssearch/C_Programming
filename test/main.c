9596a2fd004ce3b00e9640569eb8238a0eee2bad
maxim <merlinssearch@gmail.com>
2025-11-04 15:50:25 +0100

    added a test folder

---
+++ test/main.c
@@ -0,0 +1,9 @@
+#include "stdio.h"
+#include <limits.h>
+#include <math.h>
+int main() {
+  printf("Minimum Int value:%d\n", INT_MIN);
+  printf("Minimum Int value:%d\n", INT_MAX);
+  printf("%zu\n", sizeof(int) * 8);
+  return 0;
+}
